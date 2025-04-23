5f53d674837fc567f0f2c0bf8b1998c3684d2d4e
drwhoru <40565611+drwhoru@users.noreply.github.com>
2025-04-23 16:52:00 +0800

    Add src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FinancialAnalysisReport from './FinancialAnalysisReport';
+
+function App() {
+  return (
+    <div className="App">
+      <FinancialAnalysisReport />
+    </div>
+  );
+}
+
+export default App;
